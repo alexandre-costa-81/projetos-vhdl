31bbee6edd16432593fb82eff98def58731b9780
xandregc <alexandre.gcosta@gmail.com>
2015-05-05 11:55:27 -0300

    Corrigindo somador
    foi preciso corrigir o somador pois não estava adicionando o carry
    correto.

--- multiplicador/somador.vhd
+++ multiplicador/somador.vhd
@@ -13,5 +13,25 @@ end somador;
 
 architecture arq of somador is
 begin
-	soma <= '0'&(operando_a + operando_b);
+	process(operando_a, operando_b)
+	variable s: std_logic_vector(3 downto 0);
+	variable c: std_logic;
+	begin
+		c := '0';
+		
+		s(0) := operando_a(0) xor operando_b(0) xor c;
+		c := (operando_a(0) and operando_b(0)) or ( (operando_a(0) xor operando_b(0)) and c );
+		
+		s(1) := operando_a(1) xor operando_b(1) xor c;
+		c := (operando_a(1) and operando_b(1)) or ( (operando_a(1) xor operando_b(1)) and c );
+		
+		s(2) := operando_a(2) xor operando_b(2) xor c;
+		c := (operando_a(2) and operando_b(2)) or ( (operando_a(2) xor operando_b(2)) and c );
+		
+		s(3) := operando_a(3) xor operando_b(3) xor c;
+		c := (operando_a(3) and operando_b(3)) or ( (operando_a(3) xor operando_b(3)) and c );
+		
+		soma <= c&s;
+	
+	end process;
 end arq;
